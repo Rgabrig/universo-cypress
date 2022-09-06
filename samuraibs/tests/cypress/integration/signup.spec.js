d41a02f548a2e317ae24a18727eb27eb8651c67b
Raphael <raphael.gabrig@hotmail.com>
2022-09-06 17:23:23 -0300

    Utilizando biblioteca faker

--- samuraibs/tests/cypress/integration/signup.spec.js
+++ samuraibs/tests/cypress/integration/signup.spec.js
@@ -1,10 +1,10 @@
 
-
+import { faker } from '@faker-js/faker'
 
 it('deve cadastrar um novo usuário', function(){
 
     const name = 'Raphael Gabrig'
-    const email = 'raphaelgabrig@samuraibs.com'
+    const email = faker.internet.email()
     const password = '123456'
 
     cy.visit('/signup')
@@ -12,4 +12,11 @@ it('deve cadastrar um novo usuário', function(){
     cy.get('input[placeholder="Nome"]').type(name)
     cy.get('input[placeholder="E-mail"]').type(email)
     cy.get('input[placeholder="Senha"]').type(password)
+
+    cy.contains('button', 'Cadastrar').click()
+
+    cy.get('.toast')
+        .should('be.visible')
+        .find('p')
+        .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
 })
